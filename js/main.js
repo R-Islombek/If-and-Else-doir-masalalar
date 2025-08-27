00a01f8c4b3cd56b871d7a8af180eb463d14f102
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-27 23:35:18 +0500

    8-masala

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@
 
 // if (number > 0 && number % 2 !== 0 ) {
 //     console.log("musbat toq", number);
-    
+
 // } else if(number > 0 &&  number % 2 ===0) {
 //     console.log("musbat juft", number);
 // }else if(number < 0 && number % 2 !== 0){
@@ -94,5 +94,33 @@
 
 // 7-Masala;
 
+let day = prompt("Hafta kunlarini kiriting");
+
+
+switch (day) {
+    case 1:
+        console.log("Dushanba");
+        break;
+    case 2:
+        console.log("Seshanba");
+        break;
+    case 3:
+        console.log("Chorshanba");
+        break;
+    case 4:
+        console.log("Payshanba");
+        break;
+    case 5:
+        console.log("Juma");
+        break;
+    case 6:
+        console.log("Shanba");
+        break;
+    case 7:
+        console.log("Yakshanba");
+        break;
+    default:
+        console.log("1 dan 7 gacha bo'lgan son kiriting");
+}
 
 
