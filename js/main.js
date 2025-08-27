27004c1916de65c57821bae25f7cfe9429845da8
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-27 23:01:38 +0500

    2-Masala

--- js/main.js
+++ js/main.js
@@ -7,3 +7,34 @@
 // } else {
 //     alert(number - 2)
 // }
+
+// 2-Masala;
+
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0) {
+//     musbat++
+// }
+// if (a < 0) {
+//     manfiy++
+// }
+// if (b > 0) {
+//     musbat++
+// }
+// if (b < 0) {
+//     manfiy++
+// }
+// if (c > 0) {
+//     musbat++
+// }
+// if (c < 0) {
+//     manfiy++
+// }
+
+console.log("Musbat sonlar,=", musbat, "Manfiy sonlar =", manfiy);
+
