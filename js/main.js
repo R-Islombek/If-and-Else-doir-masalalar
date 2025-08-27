ec77b154fc13fbd2367c4951aea11b4a9b1d657c
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-27 23:48:46 +0500

    12-masala

--- js/main.js
+++ js/main.js
@@ -166,3 +166,34 @@
 // }else{
 //     alert(10)
 // }
+
+// 12-Masala;
+
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0) {
+//     musbat++
+// }
+// if (a < 0) {
+//     manfiy++
+// }
+// if (b > 0) {
+//     musbat++
+// }
+// if (b < 0) {
+//     manfiy++
+// }
+// if (c > 0) {
+//     musbat++
+// }
+// if (c < 0) {
+//     manfiy++
+// }
+
+// console.log("Musbat sonlar,=", musbat, "Manfiy sonlar =", manfiy);
+
